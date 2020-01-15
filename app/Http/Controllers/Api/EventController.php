80026b9e77e74a7060a81482807b2e1a2a76707f
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2020-01-15 15:13:44 +0900

    生成したモデルと画像パスをDBに保存する

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Event;
+use App\Boxel;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -10,27 +10,27 @@ use App\Http\Controllers\Controller;
 class EventController extends Controller
 {
 
-    protected $event;
+    protected $boxel;
     protected $localPath = '/Users/muramasa/ws/Misoten/';
     protected $mainProjectName = 'Rakugaki2Model_MainProcess';
     protected $clientProjectName = 'Rakugaki2Model_Client';
     protected $serverProjectName = 'Rakugaki2Model_Server';
 
-    public function __construct(Event $event) {
-        $this->event = $event;
+    public function __construct(Boxel $boxel) {
+        $this->boxel = $boxel;
     }
 
     public function index() {
-        return $this->event->all();
+        return $this->boxel->all();
     }
 
     public function add($img, $obj) {
 
-        $event = new Event();
-        $event->image = $img;
-        $event->obj = $obj;
+        $boxel = new Boxel();
+        $boxel->image = $img;
+        $boxel->obj = $obj;
 
-        $event->save();
+        $boxel->save();
     }
 
     public function generate(Request $req) {
@@ -38,18 +38,30 @@ class EventController extends Controller
         $res = []; $ret = '';
         $targetPath = $this->localPath . $this->mainProjectName;
         if($base64 = $req->base64) {
+            // base64画像を画像ファイルに変換
             $base64bin = str_replace('data:image/png;base64,', '', $base64);
             $base64bin = str_replace(' ', '+', $base64bin);
             $image = base64_decode($base64bin);
-            exec("rm -rf {$targetPath}/test_image/*");
-            // \File::put("{$targetPath}/test_image/aaa.jpg", $image);
-            file_put_contents("{$targetPath}/test_image/aaa.jpg", $image);
 
+            // 既存の画像を置換
+            exec("rm -rf {$targetPath}/test_image/*");
+            file_put_contents("{$targetPath}/test_image/model.jpg", $image);
+
+            // Client側にpix2pixで生成した画像ファイルを保存する
+            file_put_contents("{$this->localPath}{$this->clientProjectName}/output/images/{$req->file}.jpg", $image);
+            // 3Dモデルの生成及びClient側への保存
             $execFile = 'demo.py';
-            $output = '/output/models/' . 'test' . '.obj';
+            $output = "/output/models/{$req->file}.obj";
             $output = $this->localPath . $this->clientProjectName . $output;
             $cmd = "cd {$targetPath} && python {$execFile} {$output} >& /dev/null";
             exec($cmd, $res);
+
+            // ユーザー毎に画像及び3Dモデルのパスを保存
+            $boxel = new Boxel();
+            $boxel->user = $req->user;
+            $boxel->file = $req->file;
+            $boxel->save();
+
             return $res;
         }
         return abort(400);
--- database/migrations/2019_06_28_020911_create_boxels_table.php
+++ database/migrations/2019_06_28_020911_create_boxels_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateEventsTable extends Migration
+class CreateBoxelsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreateEventsTable extends Migration
      */
     public function up()
     {
-        Schema::create('events', function (Blueprint $table) {
+        Schema::create('boxels', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('image');
-            $table->string('obj');
+            // $table->string('image');
+            // $table->string('obj');
+            $table->string('user');
+            $table->string('file');
             $table->timestamps();
         });
     }
@@ -28,6 +30,6 @@ class CreateEventsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('events');
+        Schema::dropIfExists('boxels');
     }
 }
